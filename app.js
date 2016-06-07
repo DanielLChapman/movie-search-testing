62ba718203ea267246979f31f3aa7ca8916276e3
RustyRadio <danielc@my.ccsu.edu>
2016-06-06 23:33:01 -0400

    Update to CORS

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ var mongoose = require('mongoose');
 require('./models/Movies');
 
 mongoose.connect('mongodb://localhost/movies');
+var app = express();
 
+app.options('*', cors());
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
